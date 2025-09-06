36076f7e2a48fce8f79dc84a744ec0430ff05db0
Anmol Gulati <anmol.gulati.192@gmail.com>
2025-09-06 18:07:22 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,42 +2,14 @@ const express = require('express');
 const app = express();
 
 // this will match only GET API calls to /user
-app.get('/user',(req,res)=>{
-    console.log(req.query)
-    res.send({firstName:'Anmol',lastName:'Gulati'})
-})
-
-// http://localhost:3000/user?name=anmol&age=20
-// req.query = {name:'anmol',age:20}
-
-app.get('/user/:id',(req,res)=>{
-    console.log(req.params)
-    res.send({firstName:'Anmol',lastName:'Gulati'})
-})
-// http://localhost:3000/user/123
-// req.params = {id:'123'}
-
-app.get('/user/:id/book/:bookId',(req,res)=>{
-    console.log(req.params)
-    res.send({firstName:'Anmol',lastName:'Gulati'})
-})
-// http://localhost:3000/user/123/book/456
-// req.params = {id:'123',bookId:'456'}
-
-
-// this will match only POST API calls to /user
-app.post('/user',(req,res)=>{
-    res.send('Post API called')
-})
-
-// this will match only DELETE API calls to /user
-app.delete('/user',(req,res)=>{
-    res.send('Delete API called')
-})
-
-// this will match all the http methods API calls to /test
-app.use("/test",(req,res)=>{
-    res.send('Hello World anmol')
-})
+app.get('/user',(req,res,next)=>{
+    console.log('This is the first req')
+    next()
+},
+(req,res)=>{
+    console.log('This is the second callback')
+    res.send({firstName:'John',lastName:'Doe'})
+}
+)
 
 app.listen(3000)
