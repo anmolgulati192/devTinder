a72b704c4cbb024cc063b3113b643658d5a3efea
Anmol Gulati <anmol.gulati.192@gmail.com>
2025-09-06 17:40:44 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const app = express();
+
+// this will match only GET API calls to /user
+app.get('/user',(req,res)=>{
+    console.log(req.query)
+    res.send({firstName:'Anmol',lastName:'Gulati'})
+})
+
+// http://localhost:3000/user?name=anmol&age=20
+// req.query = {name:'anmol',age:20}
+
+app.get('/user/:id',(req,res)=>{
+    console.log(req.params)
+    res.send({firstName:'Anmol',lastName:'Gulati'})
+})
+// http://localhost:3000/user/123
+// req.params = {id:'123'}
+
+app.get('/user/:id/book/:bookId',(req,res)=>{
+    console.log(req.params)
+    res.send({firstName:'Anmol',lastName:'Gulati'})
+})
+// http://localhost:3000/user/123/book/456
+// req.params = {id:'123',bookId:'456'}
+
+
+// this will match only POST API calls to /user
+app.post('/user',(req,res)=>{
+    res.send('Post API called')
+})
+
+// this will match only DELETE API calls to /user
+app.delete('/user',(req,res)=>{
+    res.send('Delete API called')
+})
+
+// this will match all the http methods API calls to /test
 app.use("/test",(req,res)=>{
     res.send('Hello World anmol')
 })
-app.use((req,res)=>{
-    res.send('Hi there')
-})
+
 app.listen(3000)
